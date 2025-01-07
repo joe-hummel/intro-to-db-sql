47958c3e2b4d7f39f9d20471809c3b7f14450bbe
Joe Hummel, PhD <joe.hummel@northwestern.edu>
2025-01-06 22:39:19 -0600

    updated to new docker setup

--- docker-build.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Linux/Mac BASH script to build docker container
-#
-docker rmi sqlite3
-docker build -t sqlite3 .
--- docker-run.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Linux/Mac BASH script to run docker container
-#
-docker run -it -u user -w /home/user -v .:/home/user --rm sqlite3 bash
